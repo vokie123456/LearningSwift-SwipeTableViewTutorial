85e22fbc9749c5162d2940a88973e2d7f3661a6c
Jonathan Chiam <jczjchiam@gmail.com>
2016-03-14 15:22:45 +0800

    Completed

--- SwipeTableViewTutorial/TableViewController.swift
+++ SwipeTableViewTutorial/TableViewController.swift
@@ -30,7 +30,38 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         
+        let shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Share", handler: { (action: UITableViewRowAction!, indexPath: NSIndexPath!) -> Void in
         
+            let shareMenu = UIAlertController(title: nil, message: "Share using", preferredStyle: .ActionSheet)
+        
+            let twitterAction = UIAlertAction(title: "Twitter", style: UIAlertActionStyle.Default, handler: nil)
+        
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        
+            shareMenu.addAction(twitterAction)
+            shareMenu.addAction(cancelAction)
+            
+            self.presentViewController(shareMenu, animated: true, completion: nil)
+        })
+    
+        let rateAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Rate", handler: { (action: UITableViewRowAction!, indexPath: NSIndexPath!) -> Void in
+        
+            let rateMenu = UIAlertController(title: nil, message: "Rate this App", preferredStyle: .ActionSheet)
+            
+            let appRateAction = UIAlertAction(title: "Rate", style: UIAlertActionStyle.Default, handler: nil)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            rateMenu.addAction(appRateAction)
+            rateMenu.addAction(cancelAction)
+            
+            self.presentViewController(rateMenu, animated: true, completion: nil)
+        })
+        
+        return [shareAction, rateAction]
     }
 }
